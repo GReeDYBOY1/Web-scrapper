18df6ec4dad482336ab849e224c8f29041e3f7a4
GReeDYBOY1 <ahmedjimmyx@gmail.com>
2022-06-22 12:13:33 +0200

    UPDATED

--- scrape.py
+++ scrape.py
@@ -14,10 +14,10 @@ degrees = []
 links = []
 percentage = []
 
-for i in range(32220001, 32220291):
+for i in range(33223001, 33223096):
     print ("****" , i , "******")
-    if requests.get(f"http://app1.helwan.edu.eg/EngHelwan/HasasnUplist.asp?z_dep=%3D&z_gro=%3D&z_st_settingno=%3D&x_gro=%C7%E1%CB%C7%E1%CB%C9&x_dep=%E5%E4%CF%D3%C9+%C7%E1%DE%E6%EC+%E6%C7%E1%C7%E1%C7%CA+%C7%E1%DF%E5%D1%C8%ED%C9&z_st_name=LIKE&x_st_name=&x_st_settingno={i}&Submit=++++%C8%CD%CB++++") is not None :
-        url = requests.get(f"http://app1.helwan.edu.eg/EngHelwan/HasasnUplist.asp?z_dep=%3D&z_gro=%3D&z_st_settingno=%3D&x_gro=%C7%E1%CB%C7%E1%CB%C9&x_dep=%E5%E4%CF%D3%C9+%C7%E1%DE%E6%EC+%E6%C7%E1%C7%E1%C7%CA+%C7%E1%DF%E5%D1%C8%ED%C9&z_st_name=LIKE&x_st_name=&x_st_settingno={i}&Submit=++++%C8%CD%CB++++")
+    if requests.get(f"http://app1.helwan.edu.eg/EngHelwan/HasasnUplist.asp?z_dep=%3D&z_gro=%3D&z_st_settingno=%3D&x_gro=%C7%E1%CB%C7%E1%CB%C9&x_dep=%E5%E4%CF%D3%C9+%C7%E1%C7%E1%DF%CA%D1%E6%E4%ED%C7%CA+%E6%C7%E1%C7%CA%D5%C7%E1%C7%CA+%E6%C7%E1%CD%C7%D3%C8%C7%CA&z_st_name=LIKE&x_st_name=&x_st_settingno={i}&Submit=++++%C8%CD%CB++++") is not None :
+        url = requests.get(f"http://app1.helwan.edu.eg/EngHelwan/HasasnUplist.asp?z_dep=%3D&z_gro=%3D&z_st_settingno=%3D&x_gro=%C7%E1%CB%C7%E1%CB%C9&x_dep=%E5%E4%CF%D3%C9+%C7%E1%C7%E1%DF%CA%D1%E6%E4%ED%C7%CA+%E6%C7%E1%C7%CA%D5%C7%E1%C7%CA+%E6%C7%E1%CD%C7%D3%C8%C7%CA&z_st_name=LIKE&x_st_name=&x_st_settingno={i}&Submit=++++%C8%CD%CB++++")
         url.encoding = 'windows-1256'
         soup = bss4(url.text, 'lxml')
 
@@ -25,13 +25,6 @@ for i in range(32220001, 32220291):
             link = soup.find("tr", {"class": "ewTableRow"}).find("a").attrs['href']
             link_natiga = "http://app1.helwan.edu.eg/EngHelwan/" + link
             links.append(link_natiga)
-        # else :
-        #     url = requests.get(f"http://app1.helwan.edu.eg/EngHelwan/HasasnUplist.asp?z_dep=%3D&z_gro=%3D&z_st_settingno=%3D&x_gro=%C7%E1%CB%C7%E1%CB%C9&x_dep=%E5%E4%CF%D3%C9+%C7%E1%DE%E6%EC+%E6%C7%E1%C7%E1%C7%CA+%C7%E1%DF%E5%D1%C8%ED%C9&z_st_name=LIKE&x_st_name=%E3&x_st_settingno={i}&Submit=++++%C8%CD%CB++++")
-        #     url.encoding = 'windows-1256'
-        #     soup = bss4(url.text, 'lxml')
-        #     link = soup.find("tr", {"class": "ewTableRow"}).find("a").attrs['href']
-        #     link_natiga = "http://app1.helwan.edu.eg/EngHelwan/" + link
-        #     links.append(link_natiga)
 
 print ("\n")
 for i , l in enumerate(links):
@@ -51,28 +44,23 @@ for i , l in enumerate(links):
     rows2 = table2.findAll("font", {"size": "4"})
 
     total = 0
-    if (rows2[3].text.strip() != ""):
-        total += int(rows2[3].text.strip())
-    if (rows2[11].text.strip() != ""):
-        total += int(rows2[11].text.strip())
-    if (rows2[19].text.strip() != ""):
-        total += int(rows2[19].text.strip())
-    if (rows2[27].text.strip() != ""):
-        total += int(rows2[27].text.strip())
+    index = [3,7,11,19,27,43,51,59,67,75]
+    for b in index:
+        if (rows2[b].text.strip() != ""):
+            total += int(rows2[b].text.strip())
 
-    #total = int(rows2[3].text.strip()) + int(rows2[11].text.strip()) + int(rows2[19].text.strip()) + int(rows2[27].text.strip())
-    #print(total)
     degrees.append(total)
-    percent = truncate(((total / 650) * 100), 2)
+    percent = truncate(((total / 1500) * 100), 2)
     percentage.append(percent)
-    # print ("************************   " ,  i  , "   ************************")
+
     print(i+1,"-" ,students[i], " : ", rows2[3].text.strip(), rows2[11].text.strip(), rows2[19].text.strip(),
-          rows2[27].text.strip()
-          , "-----> ", percent , "%")
+          rows2[27].text.strip(),rows2[43].text.strip(),rows2[51].text.strip(),rows2[59].text.strip(),rows2[67].text.strip(),rows2[75].text.strip(),
+          rows2[7].text.strip(), "-----> ",total, "-----> ", percent , "%")
+
 student_row = [students, id_numbers, degrees, percentage]
 exported = zip_longest(*student_row)
 
-with open('final.csv', 'w', encoding='utf-8') as file:
+with open('tessst.csv', 'w', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(['اسم الطالب', 'رقم الجلوس', 'المجموع' ,'النسبة المئوية'])
     writer.writerows(exported)
